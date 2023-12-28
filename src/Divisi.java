7d0550e05a2240fe68f2a3e82cd1d2f858c9cc17
MALIK ARJUNA PUTRA <malikputra714@gmail.com>
2023-12-28 19:37:22 +0700

    frist commite

---
+++ src/Divisi.java
@@ -0,0 +1,10 @@
+public abstract class Divisi implements Anggaran_Divisi, Pekerjaan{
+    protected String kode_div;
+    protected String nama_div;
+    protected String jumlah_karyawan;
+
+    @Override
+    public void Anggaran_Div() {}
+    @Override
+    public void spesialis() {}
+}
